f42c1d551ecac4745326ca0951771da56e296c49
Omkar Gardas <omkar_gardas@epam.com>
2025-01-02 12:13:18 +0530

    useActionState

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Login from "./components/login";
+import LoginReact19 from "./components/login-react19";
 
 const App = () => {
   return (
     <div>
       <h1>Hello world!</h1>
       <Login />
+      <LoginReact19/>
     </div>
   );
 };
